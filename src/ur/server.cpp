de7e84594f46c25444526c8cea97b3a366437a96
Simon Rasmussen <zetlon@gmail.com>
2017-10-19 21:19:37 +0200

    Disabled TCP_NODELAY on reverse socket connection

--- src/ur/server.cpp
+++ src/ur/server.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include "ur_modern_driver/log.h"
 
-URServer::URServer(int port) : port_(port), client_(true, true)
+URServer::URServer(int port) : port_(port), client_(true, false)
 {
 }
 
